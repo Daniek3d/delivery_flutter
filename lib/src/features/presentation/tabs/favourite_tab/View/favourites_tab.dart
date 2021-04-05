02de6ed022d0ca04970821342673a125ae3dc070
Daniel <54344962+Daniek3d@users.noreply.github.com>
2021-04-04 22:06:44 -0700

    Favourite tab design

--- lib/src/features/presentation/tabs/favourite_tab/View/favourites_tab.dart
+++ lib/src/features/presentation/tabs/favourite_tab/View/favourites_tab.dart
@@ -9,8 +9,46 @@ class _FavouritesTabState extends State<FavouritesTab> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text('FavouritesTab'),
+      backgroundColor: bgGreyPage,
+      body: CustomScrollView(
+        slivers: [
+          SliverAppBar(
+            centerTitle: true,
+            leading: Text(''),
+            backgroundColor: white,
+            title: titleText(
+                text: 'My favourite',
+                // color: primary,
+                fontSize: 17,
+                fontWeight: FontWeight.w600),
+          ),
+          SliverList(
+            delegate: SliverChildListDelegate([
+              Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 10),
+                  child: Column(children: [
+                    favouritesCard(
+                        image: NetworkImage(
+                            'https://source.unsplash.com/U6-KvV7HlMk'),
+                        title: "Andy & cindy's Diner",
+                        subTitle: '87 Botsford Circle Apt',
+                        review: "4.9",
+                        rating: '(233 rating)',
+                        bottonText: 'Delivery',
+                        isFavourite: true),
+                    favouritesCard(
+                        image: NetworkImage(
+                            'https://source.unsplash.com/U6-KvV7HlMk'),
+                        title: "Andy & cindy's Diner",
+                        subTitle: '87 Botsford Circle Apt',
+                        review: "4.9",
+                        rating: '(233 rating)',
+                        bottonText: 'Delivery',
+                        isFavourite: true),
+                  ])),
+            ]),
+          )
+        ],
       ),
     );
   }
